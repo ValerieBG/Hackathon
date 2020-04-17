97d90999071caf4427abfa27dbf452e3908f58c7
Sherry Liu <sherryliu987@gmail.com>
2020-04-17 18:14:16 -0400

    copied html for chatbot formatting

--- venv/TestingPython.py
+++ venv/TestingPython.py
@@ -1,12 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+text = ""
+text = "hello i am your therapyst how are u feeling today?"
+
 @app.route("/")
 def home():
-    return "hello i am your therapyst how are u feeling today?"
+    return render_template("home.html")
 
-if __name__ == "__main__":
-    app.run()
+class Bot:
+    response = ""
+    def __init__(self):
+        self.response = "hello"
+    def get_response(self, txt):
+        return self.response
+
+bot = Bot()
+
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(bot.get_response(userText))
+
+def runApp():
+    if __name__ == "__main__":
+        app.run()
+
+def main():
+    runApp()
+
+main()
 
 
